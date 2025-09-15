4ed2c67e8228dce60d43f53bb9a262ce74fb139f
MTristano <Tristanom@gmail.com>
2025-09-14 21:38:14 -0400

    Update web build with web-friendly main.dart

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2929292768"
+    serviceWorkerVersion: "3255236901"
   }
 });
